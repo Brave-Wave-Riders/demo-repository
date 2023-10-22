fdc488aa43d4b4c0c27571d1f991699f1040f956
dryFirewood-young <2207982619@qq.com>
2023-10-22 10:55:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+)
 
+func init() {
+	go http.ListenAndServe(":8888", nil)
+}
 func main() {
 	fmt.Println("hello,world")
+	select {}
 }
